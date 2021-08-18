3030ce24f6ffe2b503cc31aec213034278c94e57
Felipe Tina da Silva <86685364+AlphaProgramador-Edutech@users.noreply.github.com>
2021-08-18 11:17:04 -0300

    Add files via upload

---
+++ Advinhacao.py
@@ -0,0 +1,24 @@
+print("--------------------------------")
+print("Bem vindo ao jogo de Advinhação!")
+print("--------------------------------")
+
+import random
+numero_secreto = random.randrange(1, 100)
+print("Dica: o número secreto está entre", numero_secreto-random.randrange(1, 6) ,"e", numero_secreto+random.randrange(1, 6))
+tentativas = 1
+
+while(tentativas <= 3):
+    chute = input("Chute um número:")
+    numeroChutado = int(chute)
+    if(numero_secreto == numeroChutado):
+        print("Parabéns, você acertou!")
+        break #<--- Usei a função "break" como parametro de quebra do loop#
+    else:
+        if(numeroChutado > numero_secreto):
+            print("Sinto muito, você errou! Seu chute saiu mais alto")
+        else:
+            print("Sinto muito, você errou! Seu chute saiu mais baixo")
+            print("Tentativa", tentativas ,"de 3")
+            tentativas= tentativas + 1
+
+print("FIM DO JOGO!")
